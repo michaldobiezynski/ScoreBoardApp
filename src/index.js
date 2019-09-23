978e6fc3f04f43eee7929f600b29263ca7c60ce4
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-09-23 16:58:20 +0100

    created a 'player' component

--- src/index.js
+++ src/index.js
@@ -16,7 +16,25 @@ function Header() {
     );
 }
 
+const Player = () => {
+    return (
+      <div className="player">
+          <span className="player-name">
+              Michal
+          </span>
+
+          <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <span className="counter-score">35</span>
+              <button className="counter-action increment"> + </button>
+          </div>
+      </div>
+    );
+}
+
 ReactDOM.render(
-    <Header />,
+    <Player />,
+
     document.getElementById('root')
 );
+
