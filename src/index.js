74f93f7497e8cb694e66986cbfaaf8d7c939bf95
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-09-23 16:46:15 +0100

    added some css styling and cleared app.js and rendered a component in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
 serviceWorker.unregister();
+
+
+function Header() {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    );
+}
+
+ReactDOM.render(
+    <Header />,
+    document.getElementById('root')
+);
