6cbfd61a747afcd9c8819505f61dbe47b3d6dc22
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-09-24 17:27:09 +0100

    created custom method and added on click event to one of the buttons

--- src/index.js
+++ src/index.js
@@ -62,12 +62,19 @@ class Counter extends React.Component {
         };
     }
 
+    incrementScore() {
+        this.setState({
+            score: this.state.score + 1
+        });
+    }
+
     render() {
         return (
             <div className="counter">
                 <button className="counter-action decrement"> - </button>
-                <span className="counter-score">{this.props.score}</span>
-                <button className="counter-action increment"> + </button>
+                <span className="counter-score">{this.state.score}</span>
+                <button className="counter-action increment"
+                        onClick={this.incrementScore}> + </button>
             </div>
         );
     }
@@ -87,7 +94,6 @@ const App = (props) => {
                 player =>
                     <Player
                 name={player.name}
-                score={player.score}
                     key={player.id.toString()}
                     />
             )}
