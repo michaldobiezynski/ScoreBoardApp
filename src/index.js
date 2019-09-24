000d36fcb34cba791db2226211d417f2a80708c6
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-09-24 17:41:58 +0100

    added callback to arrow functions which are triggered by buttons

--- src/index.js
+++ src/index.js
@@ -63,14 +63,18 @@ class Counter extends React.Component {
     }
 
     incrementScore = () => {
-        this.setState({
-            score: this.state.score + 1
-        });
+        this.setState( prevState => {
+            return {
+                score: prevState.score + 1
+            }
+        })
     }
 
     decrementScore = () => {
-        this.setState( {
-            score: this.state.score - 1
+        this.setState( prevState => {
+            return {
+                score: prevState.score - 1
+            }
         })
     }
 
