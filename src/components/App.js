a2e2b24a49dbff22b6f7f123718f51bdcb5f0733
dobimi <michal.dobiezynski@woodmac.com>
2019-10-11 15:50:14 +0100

    moved Counter component to it's own class

--- src/components/App.js
+++ src/components/App.js
@@ -6,47 +6,6 @@ import * as serviceWorker from './serviceWorker.js';
 
 serviceWorker.unregister();
 
-
-class Counter extends React.Component {
-
-    constructor() {
-        super()
-        this.state = {
-            score: 0
-        };
-    }
-
-    incrementScore = () => {
-        this.setState( prevState => {
-            return {
-                score: prevState.score + 1
-            }
-        })
-    }
-
-    decrementScore = () => {
-        this.setState( prevState => {
-            return {
-                score: prevState.score - 1
-            }
-        })
-    }
-
-    render() {
-        return (
-            <div className="counter">
-                <button className="counter-action decrement"
-                        onClick={this.decrementScore}> - </button>
-                <span className="counter-score">{this.state.score}</span>
-                <button className="counter-action increment"
-                        onClick={this.incrementScore}> + </button>
-            </div>
-        );
-    }
-}
-
-
-
 class App extends React.Component {
 
     state = {
