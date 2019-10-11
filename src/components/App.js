fefd913c51af8cd8ba5c8240f42352118e6fdb9c
dobimi <michal.dobiezynski@woodmac.com>
2019-10-11 13:43:59 +0100

    moved App.js to the new folder

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../App.css';
 import * as serviceWorker from './serviceWorker';
-import App from "./index";
 
 serviceWorker.unregister();
 
