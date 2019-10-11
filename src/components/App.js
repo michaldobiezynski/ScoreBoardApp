813f4d3bcec594fac51090365c41a8865426be36
dobimi <michal.dobiezynski@woodmac.com>
2019-10-11 15:09:39 +0100

    reorganized code a bit more

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import '../App.css';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker.js';
 
 serviceWorker.unregister();
 
