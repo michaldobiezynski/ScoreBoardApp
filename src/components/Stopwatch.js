19ad8ba3a62560c12c9f9b86a4ed97ddc88fad52
dobimi <michal.dobiezynski@woodmac.com>
2019-10-17 08:55:18 +0100

    created a small file to prepend the node path and created something like if statment to change the button displayed in stopwatche depending if it is running or no

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
 
 class Stopwatch extends Component {
+
+    state = {
+      isRunning: false
+    };
+
     render() {
         return (
             <div className="stopwatch">
                 <h2>Stopwatch</h2>
                 <span className="stopwatch-time">0</span>
-                <button>Start</button>
+                <button>
+                    {this.state.isRunning ? 'Stop' : 'Start'}
+                </button>
                 <button>Reset</button>
             </div>
         );
